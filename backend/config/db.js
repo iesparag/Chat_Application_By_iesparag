03d091c89ba52b027b274429b107fea3ae9f9931
Parag Jain <iesparagjain@gmail.com>
2023-03-22 13:00:50 +0530

    chat-app-by-iesparag

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+const connectDB = async () => {
+    try{
+       const conn = await mongoose.connect(process.env.MONGO_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+       })
+
+       console.log(`mongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    }catch(error){
+      console.log(`Error: ${error.message}`.red.bold);
+      process.exit();
+    }
+}
+
+module.exports = connectDB;
